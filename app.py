2894ca6e0dda3c3801951d33053145f9b37a70fc
Parfait Detchenou <edetchenou@outlook.com>
2024-10-31 01:04:36 +0100

    More modular code and delay card analysis

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
+import json
 import warnings
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
+import numpy as np
 import cv2
+import requests
 from ultralytics import YOLO
 from doctr.models import ocr_predictor
 import pika
+import pika.credentials
 from pika.channel import Channel
 
 from extractors.cip import CIPExtractor
@@ -24,56 +28,106 @@ def is_valid(user):
 
 
 def log(user: dict, channel: Channel):
-    channel.basic_publish(exchange="", routing_key="logs", body=user)
+    channel.basic_publish(exchange="", routing_key="logs", body=json.dumps(user))
+    with open("logs.txt", "a") as f:
+        f.write(json.dumps(user) + "\n")
 
 
-def main():
-    pass
+def get_image_from_stream(capture):
+    ret, frame = capture.read()
+    return ret, frame
 
 
-if __name__ == "__main__":
-    should_run = True
+def main(channel: Channel, capture: cv2.VideoCapture):
     is_currently_processing = False
 
     segmentation_model = YOLO("yolo11n-seg.pt")
-    model = ocr_predictor(pretrained=True)
+    model = model = ocr_predictor(
+        det_arch="db_resnet50", reco_arch="crnn_vgg16_bn", pretrained=True
+    )
 
-    capture = cv2.VideoCapture(0)
+    card_counter = 0
+    while True:
+        ret, frame = get_image_from_stream(capture)
 
-    CARD_SLUG = "book"
-
-    is_card_detected = False
-
-    connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
-    channel = connection.channel()
-    channel.queue_declare(queue="logs")
-
-    while should_run:
-        ret, frame = capture.read()
+        frame = cv2.resize(frame, (640, 480))
 
         if not ret:
             continue
 
-        results = segmentation_model(frame)
-        annotated_frame = results[0].plot()
-        for result in results:
-            for box in result.boxes:
-                category = segmentation_model.names.get(box.cls.item())
-                if category == CARD_SLUG:
-                    is_card_detected = True
-                    break
+        annotated_frame, prediction = get_card(segmentation_model, frame)
+        if not is_currently_processing and prediction is not None:
+            card_counter += 1
 
-        if is_card_detected and not is_currently_processing:
-            document = model([frame])
-            try:
-                ex = get_extractor("cip")(document)
-                user = ex.extract()
-            except Exception as e:
-                print(e)
-            else:
-                if user is is_valid(user):
-                    log(user, channel)
-                    should_run = False
+            if card_counter >= 20:
+                is_currently_processing = True
+
+                x1, y1, x2, y2 = [int(i) for i in prediction]
+                card = frame[y1:y2, x1:x2]
+
+                document = model([card])
+
+                try:
+                    ex = get_extractor("cip")(document)
+                    user = ex.extract()
+                except Exception as e:
+                    print(e)
+                else:
+                    if user:
+                        log(user, channel)
+                is_currently_processing = False
+                card_counter = 0
 
         cv2.imshow("frame", annotated_frame)
         cv2.waitKey(1)
+
+
+def is_included_in(box1, box2):
+    x1, y1, x2, y2 = box1
+    x3, y3, x4, y4 = box2
+    return x1 >= x3 and y1 >= y3 and x2 <= x4 and y2 <= y4
+
+
+def get_card(model, frame):
+    CARD_IDS = [73, 72, 67]
+    PHOTO_ID = 0
+
+    results = model(frame)
+    annotated_frame = results[0].plot()
+    card_box = None
+    photo_box = None
+
+    for result in results:
+        for box in result.boxes:
+            if box.cls.item() in CARD_IDS:
+                card_box = box.xyxy[0]
+            if box.cls.item() == PHOTO_ID:
+                photo_box = box.xyxy[0]
+
+            if (
+                photo_box is not None
+                and card_box is not None
+                and is_included_in(photo_box, card_box)
+            ):
+                return (annotated_frame, card_box)
+    return annotated_frame, None
+
+
+if __name__ == "__main__":
+    capture = cv2.VideoCapture("http://10.61.16.61:8080/video")
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            "10.61.16.131",
+            credentials=pika.PlainCredentials("peace", "peace"),
+        )
+    )
+    channel = connection.channel()
+    channel.queue_declare(queue="logs")
+
+    try:
+        main(channel, capture)
+    except KeyboardInterrupt:
+        connection.close()
+        capture.release()
+        cv2.destroyAllWindows()
--- extractors/cip.py
+++ extractors/cip.py
@@ -1,3 +1,5 @@
+import re
+
 from datetime import datetime as dt
 import dateparser
 
@@ -15,16 +17,19 @@ class CIPExtractor(BaseExtractor):
             "Nom:": "last_name",
             "Prénom": "first_name",
             "Date de naissance": "birth_date",
+            "Numéro de": "phone",
         }
 
         for i, line in enumerate(self._block.lines):
             sentence = " ".join([word.value for word in line.words])
-            for key in FIELD_MAP:
+            for key, field in FIELD_MAP.items():
                 if key in sentence:
-                    val = self.get_line(i + 1)
-                    if hasattr(self, f"clean_{FIELD_MAP[key]}"):
-                        val = getattr(self, f"clean_{FIELD_MAP[key]}")(val)
+                    if field == "phone":
+                        val = self.get_line_value(i, field)
+                    else:
+                        val = self.get_next_line_value(i, field)
                     user[FIELD_MAP[key]] = val
+
             sentences.append(sentence)
         if user:
             filename = f"runs/{self.slug}/{dt.now().strftime('%Y%m%d-%H:%M:%S')}.txt"
@@ -33,4 +38,20 @@ class CIPExtractor(BaseExtractor):
         return user
 
     def clean_birth_date(self, birth_date):
-        return dateparser.parse(birth_date).date()
+        parsed = dateparser.parse(birth_date)
+        if parsed:
+            return parsed.strftime("%Y-%m-%d")
+
+    def clean_phone(self, phone):
+        search = re.search(r"\d{8}", phone)
+        if search:
+            return search.group()
+
+    def get_next_line_value(self, line, field):
+        return self.get_line_value(line + 1, field)
+
+    def get_line_value(self, line, field):
+        val = self.get_line(line)
+        if hasattr(self, f"clean_{field}"):
+            val = getattr(self, f"clean_{field}")(val)
+        return val
